b24acc6e4ba7a0f345a38eba1287bb298c7ab185
bilalkocoglu <bilal.kocoglu@kgteknoloji.com>
2020-07-07 14:39:01 +0300

    first commit

---
+++ src/main/java/com/kstech/requeks/RequeksApplication.java
@@ -0,0 +1,13 @@
+package com.kstech.requeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequeksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kstech/requeks/RequeksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kstech.requeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
